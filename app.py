a36600a5278212f9d4c05886140977ff2f31b6aa
James Bremner <j.bremner@ymail.com>
2025-04-04 12:28:52 +0100

    add create menu function

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import constants
 import copy
 
-
 teams = constants.TEAMS 
 players = constants.PLAYERS
 
@@ -31,8 +30,58 @@ def balance_teams():
   panthers = [full_team[i] for i in range(6)]
   bandits = [full_team[i] for i in range(6, 12)]
   warriors = [full_team[i] for i in range(12, 18)]
-  
+  return panthers, bandits, warriors
+#display team's name
+# total number of players on that team
+# player's names on that team
+
+teams = balance_teams()
+
+
+def create_menu():
+   print("Basketball team stats tool \n *** Menu *** \n Here are your choices: \n A) Display team stats \n B) Quit")
+   first_option = input("Enter an option  ")
+   # if user chooses A - display teams names
+   # Ask user to pick team A, B or C
+   if first_option.lower() == "a":
+      #display_stats()
+      print("A) Panthers \n B) Bandits \n C) Warriors")
+      team_choice = input("Enter an option  ")
+      teams = balance_teams()
+      if team_choice.lower() == "a":
+        #display stats of Panthers
+        print("Panthers \n ------ Stats")
+        display_stats(teams[0])
+      elif team_choice.lower() == "b":
+        #display stats of Bandits
+        display_stats(teams[1])
+      else: 
+        #display stats of Warriors
+        display_stats(teams[2])
+      
+      
+   # Display team stats that the user picked
+   print("Press Enter to continue")
+
+#loops through a team and outputs the player names in a list
+def showPlayerName(team):
+  player_names = []
+  for player in team:
+    player_names.insert(0, player['name'])
+
+  return player_names
+
+#function to display the team stats
+def display_stats(team):
+   #how to show the team name?
+   print(team, "stats \n ----------")
+   print("Total Players: ", len(team))
+   #Loop through team and display names
+   print("Players on the team:")
+
+#display_stats(constants.PLAYERS)
 
 if __name__ == "__main__":
     clean_data(constants.PLAYERS)
     balance_teams()
+    create_menu()
